8381a59673d28a1fe71d56380d971db1853ba2f8
Will Crichton <wcrichto@cs.stanford.edu>
2021-07-20 18:23:26 -0700

    Final fix

--- examples/slicing/build.cjs
+++ examples/slicing/build.cjs
@@ -1,11 +1,25 @@
 const esbuild = require("esbuild");
 const fs = require("fs");
+const path = require("path");
 const { program } = require("commander");
 
 program.option("-w, --watch");
 program.option("-p, --prod");
 program.parse(process.argv);
 const options = program.opts();
+
+// TODO: find better way to avoid imports on peerDependencies resolving to 
+//    reactex/node_modules
+const plugin = {
+  name: "test",
+  setup(build) {
+    build.onResolve({ filter: /^@codemirror/ }, (args) => ({
+      path: `${path.resolve(__dirname)}/node_modules/${
+        args.path
+      }/dist/index.js`,
+    }));
+  },
+};
 
 esbuild
   .build({
@@ -24,6 +38,7 @@ esbuild
       ".bib": "text",
     },
     outdir: "dist",
+    plugins: [plugin],
   })
   .then(() => {
     fs.copyFileSync("src/index.html", "dist/index.html");
