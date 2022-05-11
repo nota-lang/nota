2526c60a04d5c79a2a4269256b74b9fad2f11afe
Will Crichton <wcrichto@cs.stanford.edu>
2022-05-10 22:30:04 -0700

    Change syntax highlighting to work for latest lezer

--- packages/nota-syntax/lib/index.ts
+++ packages/nota-syntax/lib/index.ts
@@ -1,6 +1,7 @@
 export { tryParse } from "./parse.js";
 export { translate, translateAst, Translator, optimizePlugin } from "./translate.js";
-export { nota, CodeTag } from "./language.js";
+export { nota } from "./language.js";
+export { CodeTag } from "./highlight.js";
 export * as babelPolyfill from "./babel-polyfill.js";
 
 //@ts-ignore
--- packages/nota-syntax/lib/language.ts
+++ packages/nota-syntax/lib/language.ts
@@ -1,4 +1,4 @@
-import { Tag, styleTags, tags as t } from "@lezer/highlight";
+import { tags as t } from "@lezer/highlight";
 import {
   HighlightStyle,
   LRLanguage,
@@ -10,79 +10,17 @@ import {
   foldNodeProp,
   indentNodeProp,
   syntaxHighlighting,
+  defaultHighlightStyle,
 } from "@codemirror/language";
 
 import { autocomplete } from "./autocomplete.js";
 //@ts-ignore
 import { parser } from "./nota.grammar.js";
+import { CodeTag } from "./highlight.js";
 
-export let CodeTag = Tag.define();
 export let notaLanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
-      styleTags({
-        "get set async static": t.modifier,
-        "for while do if else switch try catch finally return throw break continue default case":
-          t.controlKeyword,
-        "in of await yield void typeof delete instanceof": t.operatorKeyword,
-        "export import let var const function class extends": t.definitionKeyword,
-        "with debugger from as new": t.keyword,
-        TemplateString: t.special(t.string),
-        Super: t.atom,
-        BooleanLiteral: t.bool,
-        this: t.self,
-        null: t.null,
-        Star: t.modifier,
-        VariableName: t.variableName,
-        "CallExpression/VariableName": t.function(t.variableName),
-        VariableDefinition: t.definition(t.variableName),
-        Label: t.labelName,
-        PropertyName: t.propertyName,
-        PrivatePropertyName: t.special(t.propertyName),
-        "CallExpression/MemberExpression/PropertyName": t.function(t.propertyName),
-        "FunctionDeclaration/VariableDefinition": t.function(t.definition(t.variableName)),
-        "ClassDeclaration/VariableDefinition": t.definition(t.className),
-        PropertyDefinition: t.definition(t.propertyName),
-        PrivatePropertyDefinition: t.definition(t.special(t.propertyName)),
-        UpdateOp: t.updateOperator,
-        LineComment: t.lineComment,
-        BlockComment: t.blockComment,
-        Number: t.number,
-        String: t.string,
-        ArithOp: t.arithmeticOperator,
-        LogicOp: t.logicOperator,
-        BitOp: t.bitwiseOperator,
-        CompareOp: t.compareOperator,
-        RegExp: t.regexp,
-        Equals: t.definitionOperator,
-        "Arrow : Spread": t.punctuation,
-        "( )": t.paren,
-        "[ ]": t.squareBracket,
-        "{ }": t.brace,
-        ".": t.derefOperator,
-        ", ;": t.separator,
-
-        TypeName: t.typeName,
-        TypeDefinition: t.definition(t.typeName),
-        "type enum interface implements namespace module declare": t.definitionKeyword,
-        "abstract global Privacy readonly override": t.modifier,
-        "is keyof unique infer": t.operatorKeyword,
-
-        JSXAttributeValue: t.attributeValue,
-        JSXText: t.content,
-        "JSXStartTag JSXStartCloseTag JSXSelfCloseEndTag JSXEndTag": t.angleBracket,
-        "JSXIdentifier JSXNameSpacedName": t.tagName,
-        "JSXAttribute/JSXIdentifier JSXAttribute/JSXNameSpacedName": t.attributeName,
-
-        Text: t.content,
-        CommandName: t.variableName,
-        // "CommandNamedArg/Number": t.definitionKeyword,
-        At: t.definitionKeyword,
-        Pct: t.definitionKeyword,
-        Hash: t.definitionKeyword,
-        "NotaExpression/...": CodeTag,
-        "NotaStatement/...": CodeTag,
-      }),
       indentNodeProp.add({
         IfStatement: continuedIndent({ except: /^\s*({|else\b)/ }),
         TryStatement: continuedIndent({ except: /^\s*({|catch\b|finally\b)/ }),
@@ -136,12 +74,13 @@ let notaCompletion = notaLanguage.data.of({
 
 let notaStyle = HighlightStyle.define([
   { tag: t.variableName, color: "#256" },
-  // { tag: CodeTag, background: "#f5f5f5" },
+  { tag: CodeTag, class: "nota-editor-code" },
   { tag: t.content, class: "nota-editor-text" },
 ]);
 
 export let nota = () =>
   new LanguageSupport(notaLanguage, [
     notaCompletion,
-    syntaxHighlighting(notaStyle, { fallback: true }),
+    syntaxHighlighting(notaStyle),
+    syntaxHighlighting(defaultHighlightStyle),
   ]);
